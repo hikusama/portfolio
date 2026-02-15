ead933f95570d1cea1f5cb4554b0281ee1308ec5
Hikusama <leomessi25k@gmail.com>
2026-02-15 16:30:06 +0800

    skillsntech component

--- components/skillsAndTech.tsx
+++ components/skillsAndTech.tsx
@@ -1,132 +1,154 @@
+"use client";
 
+import LaserFlow from '@/components/LaserFlow';
 
-
-export default function SkillsAndTech() {
+export default function SkillsAndTech({ isPlayed }: { isPlayed: boolean }) {
     return (
         <>
-            <section>
-                <div>
-                    <h1><span><i className="fa-solid fa-head-side-virus"></i></span> Skills & Technologies</h1>
-                    <p><span><i className="fas fa-circle"></i></span> Proficient in modern web and application development technologies, with experience in full-stack development and development tools.</p>
+            <div className="sknt grid">
+                <div className='backlight' >
+                    {/* {
+                        !isPlayed &&
+                        <LaserFlow
+                            wispDensity={1.6}
+                            flowSpeed={1.3}
+                            verticalSizing={10}
+                            horizontalSizing={5}
+                            fogIntensity={2}
+                            fogScale={0.15}
+                            wispSpeed={18}
+                            wispIntensity={13.5}
+                            flowStrength={0.15}
+                            decay={1.1}
+                            horizontalBeamOffset={0.3}
+                            verticalBeamOffset={-0.5}
+                        />
+                    } */}
                 </div>
-                <div>
+                <section style={{gridArea: '1/1'}}>
+                    <div>
+                        <h1><span><i className="fa-solid fa-head-side-virus"></i></span> Skills & Technologies</h1>
+                        <p><span><i className="fas fa-circle"></i></span> Proficient in modern web and application development technologies, with experience in full-stack development and development tools.</p>
+                    </div>
+                    <div>
 
-                    <ol>
-                        <div>
-                            <span><i className="fa-solid fa-flag"></i></span> Programming  & Markup
-                        </div>
-                        <ul>
-                            <li>
-                                <i className="fa-brands fa-php"></i> PHP
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-square-js"></i> JavaScript
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-html5"></i> HTML
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-css3-alt"></i> CSS
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-database"></i> SQL
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-java"></i> Java
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-copyright"></i> C & C++
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-dart-lang"></i> Dart
-                            </li>
-                        </ul>
-                    </ol>
+                        <ol>
+                            <div>
+                                <span><i className="fa-solid fa-flag"></i></span> Programming  & Markup
+                            </div>
+                            <ul>
+                                <li>
+                                    <i className="fa-brands fa-php"></i> <p>PHP</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-square-js"></i> <p>JavaScript</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-html5"></i> <p>HTML</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-css3-alt"></i> <p>CSS</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-database"></i> <p>SQL</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-java"></i> <p>Java</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-copyright"></i> <p>C & C++</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-dart-lang"></i> <p>Dart</p>
+                                </li>
+                            </ul>
+                        </ol>
 
-                    <ol>
-                        <div>
-                            <span><i className="fa-solid fa-folder-tree"></i></span> Technologies & Frameworks
-                        </div>
-                        <ul>
-                            <li>
-                                <i className="fa-brands fa-react"></i> React
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-flutter"></i> Flutter
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-node"></i> Node.js
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-laravel"></i> Laravel
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-database"></i> MySQL
-                            </li>
-                        </ul>
-                    </ol>
-                    <ol>
-                        <div>
-                            <span><i className="fas fa-screwdriver-wrench"></i></span> Tools & Platforms
-                        </div>
-                        <ul>
-                            <li>
-                                <i className="fa-brands fa-docker"></i> Docker
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-figma"></i> Figma
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-vscode"></i> VSCode
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-github"></i> GitHub
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-git-alt"></i> Git
-                            </li>
-                            <li>
-                                <img src="/images/composer.png" /> Composer
-                            </li>
-                            <li>
-                                <img src="/images/tailwind.png" /> Tailwind
-                            </li>
-                            <li>
-                                <img src="/images/postman.png" /> Postman
-                            </li>
-                            <li>
-                                <img src="/images/xampp.png" /> XAMPP
-                            </li>
-                        </ul>
-                    </ol>
+                        <ol>
+                            <div>
+                                <span><i className="fa-solid fa-folder-tree"></i></span> Technologies & Frameworks
+                            </div>
+                            <ul>
+                                <li>
+                                    <i className="fa-brands fa-react"></i> <p>React</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-flutter"></i> <p>Flutter</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-node"></i> <p>Node.js</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-laravel"></i> <p>Laravel</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-database"></i> <p>MySQL</p>
+                                </li>
+                            </ul>
+                        </ol>
+                        <ol>
+                            <div>
+                                <span><i className="fas fa-screwdriver-wrench"></i></span> Tools & Platforms
+                            </div>
+                            <ul>
+                                <li>
+                                    <i className="fa-brands fa-docker"></i> <p>Docker</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-figma"></i> <p>Figma</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-vscode"></i> <p>VSCode</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-github"></i> <p>GitHub</p>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-git-alt"></i> <p>Git</p>
+                                </li>
+                                <li>
+                                    <img src="/images/composer.png" /> <p>Composer</p>
+                                </li>
+                                <li>
+                                    <img src="/images/tailwind.png" /> <p>Tailwind</p>
+                                </li>
+                                <li>
+                                    <img src="/images/postman.png" /> <p>Postman</p>
+                                </li>
+                                <li>
+                                    <img src="/images/xampp.png" /> <p>XAMPP</p>
+                                </li>
+                            </ul>
+                        </ol>
 
-                    <ol>
-                        <div>
-                            <span><i className="fa-solid fa-building-circle-check"></i></span> Soft Skills & Other Competencies
-                        </div>
-                        <ul>
-                            <li>
-                                <i className="fa-solid fa-puzzle-piece"></i> Problem-Solving
-                            </li>
-                            <li>
-                                <i className="fa-brands fa-leanpub"></i> Self-Learning/Curiosity
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-code-branch"></i> Version Controll
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-computer"></i> Local Development Environment
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-network-wired"></i> Networking Basics
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-database"></i> Database Operations
-                            </li>
-                        </ul>
-                    </ol>
-                </div>
-            </section>
+                        <ol className='sft'>
+                            <div>
+                                <span><i className="fa-solid fa-building-circle-check"></i></span> Soft Skills & Other Competencies
+                            </div>
+                            <ul>
+                                <li>
+                                    <i className="fa-solid fa-puzzle-piece"></i> Problem-Solving
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-leanpub"></i> <p>Self-Learning/Curiosity</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-code-branch"></i> <p>Version Controll</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-computer"></i> <p>Local Development Environment</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-network-wired"></i> <p>Networking Basics</p>
+                                </li>
+                                <li>
+                                    <i className="fa-solid fa-database"></i> <p>Database Operations</p>
+                                </li>
+                            </ul>
+                        </ol>
+                    </div>
+                </section>
+            </div>
         </>
     );
 }
